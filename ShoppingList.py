f0a9d820891ec9e6079080d892ec6ae380c0d170
DataAnalysisSOH <105328921+DataAnalysisSOH@users.noreply.github.com>
2022-06-23 11:39:13 -0400

    Add files via upload

---
+++ ShoppingList.py
@@ -0,0 +1,7 @@
+AnnaShoppingList = ['Apple', 'Orange', 'Potatoes']
+LucyShoppingList = ['Strawberry', 'Soda']
+IanShoppingList = ['Basketball', 'Dance shoes']
+
+print('AnnaShoppingList: ', AnnaShoppingList)
+print('LucyShoppingList: ', LucyShoppingList)
+print('IanShoppingList: ', IanShoppingList)
