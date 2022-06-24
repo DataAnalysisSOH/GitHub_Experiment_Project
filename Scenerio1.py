0d85bed46518384b42b8f9a3d5529be70bc9ae6f
DataAnalysisSOH <105328921+DataAnalysisSOH@users.noreply.github.com>
2022-06-24 16:52:23 -0400

    version1 #6

--- Scenerio1.py
+++ Scenerio1.py
@@ -1,5 +1,5 @@
 # Jun 24, 2022
-# Version3
+# Version1
 
 # 1
 print("This is first message!")
@@ -12,9 +12,3 @@ print("This is seventh message!")
 print("This is eighth message!")
 print("This is ninth message!")
 print("This is tenth message!")
-# 2
-print("Lucy added eleventh message!")
-# 2.5
-print("Lucy fixed the bug")
-# 3
-print("I merged from master and work on version3..")
